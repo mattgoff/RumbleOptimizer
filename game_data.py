62c14644f83da424decd3b6afdba2410c4dadb2c
Matt Goff <matt@goff.us>
2024-06-30 12:32:44 -0500

    cleanup

--- game_data.py
+++ game_data.py
@@ -4,22 +4,22 @@ from typing import Optional
 
 
 @dataclass
-class Card:
+class NameAndPriority:
     name: str
     weight: int
 
 
-class NodeTypes(enum.Enum):
-    ARCANE_ENERGY = Card("arcane_energy", 1)
-    CHARACTER = Card("character", 2)
-    TOME = Card("tome", 6)
-    CORE = Card("core", 10)
-    GOLD = Card("gold", 15)
+class NodeType(enum.Enum):
+    ARCANE_ENERGY = NameAndPriority("arcane_energy", 1)
+    CHARACTER = NameAndPriority("character", 2)
+    TOME = NameAndPriority("tome", 6)
+    CORE = NameAndPriority("core", 10)
+    GOLD = NameAndPriority("gold", 15)
 
 
 @dataclass
 class Node:
-    node_name: NodeTypes
+    node_name: NodeType
     node_cost: int
     gold_value: int = None
     arcane_value: int = None
@@ -57,52 +57,52 @@ def create_link(upper_row, direction, lower_row):
                 link_nodes(upper_row[i], direction, lower_row[i])
 
 
+row0_nodes = [
+    create_node(NodeType.ARCANE_ENERGY, 500, arcane_value=200),
+    create_node(NodeType.CHARACTER, 200),
+    create_node(NodeType.CHARACTER, 200),
+    create_node(NodeType.GOLD, 400, gold_value=75),
+]
 row1_nodes = [
-    create_node(NodeTypes.ARCANE_ENERGY, 500, arcane_value=200),
-    create_node(NodeTypes.CHARACTER, 200),
-    create_node(NodeTypes.CHARACTER, 200),
-    create_node(NodeTypes.GOLD, 400, gold_value=75),
+    create_node(NodeType.GOLD, 800, gold_value=150),
+    create_node(NodeType.CHARACTER, 400),
+    create_node(NodeType.TOME, 800),
 ]
 row2_nodes = [
-    create_node(NodeTypes.GOLD, 800, gold_value=150),
-    create_node(NodeTypes.CHARACTER, 400),
-    create_node(NodeTypes.TOME, 800),
+    create_node(NodeType.ARCANE_ENERGY, 2000, arcane_value=250),
+    create_node(NodeType.CHARACTER, 500),
+    create_node(NodeType.CHARACTER, 500),
+    create_node(NodeType.ARCANE_ENERGY, 2000, arcane_value=250),
 ]
 row3_nodes = [
-    create_node(NodeTypes.ARCANE_ENERGY, 2000, arcane_value=250),
-    create_node(NodeTypes.CHARACTER, 500),
-    create_node(NodeTypes.CHARACTER, 500),
-    create_node(NodeTypes.ARCANE_ENERGY, 2000, arcane_value=250),
+    create_node(NodeType.CHARACTER, 800),
+    create_node(NodeType.CHARACTER, 6000),
+    create_node(NodeType.CHARACTER, 800),
 ]
 row4_nodes = [
-    create_node(NodeTypes.CHARACTER, 800),
-    create_node(NodeTypes.CHARACTER, 6000),
-    create_node(NodeTypes.CHARACTER, 800),
+    create_node(NodeType.CORE, 20000),
+    create_node(NodeType.TOME, 4400),
+    create_node(NodeType.CHARACTER, 3500),
+    create_node(NodeType.ARCANE_ENERGY, 6000, arcane_value=500),
 ]
 row5_nodes = [
-    create_node(NodeTypes.CORE, 20000),
-    create_node(NodeTypes.TOME, 4400),
-    create_node(NodeTypes.CHARACTER, 3500),
-    create_node(NodeTypes.ARCANE_ENERGY, 6000, arcane_value=500),
+    create_node(NodeType.CHARACTER, 2000),
+    create_node(NodeType.GOLD, 4000, gold_value=300),
+    create_node(NodeType.ARCANE_ENERGY, 6000, arcane_value=700),
 ]
 row6_nodes = [
-    create_node(NodeTypes.CHARACTER, 2000),
-    create_node(NodeTypes.GOLD, 4000, gold_value=300),
-    create_node(NodeTypes.ARCANE_ENERGY, 6000, arcane_value=700),
+    create_node(NodeType.CHARACTER, 11000),
+    create_node(NodeType.ARCANE_ENERGY, 11000, arcane_value=1000),
+    create_node(NodeType.CHARACTER, 11000),
+    create_node(NodeType.TOME, 15000),
 ]
 row7_nodes = [
-    create_node(NodeTypes.CHARACTER, 11000),
-    create_node(NodeTypes.ARCANE_ENERGY, 11000, arcane_value=1000),
-    create_node(NodeTypes.CHARACTER, 11000),
-    create_node(NodeTypes.TOME, 15000),
-]
-row8_nodes = [
-    create_node(NodeTypes.CHARACTER, 20000),
-    create_node(NodeTypes.GOLD, 11000, gold_value=450),
-    create_node(NodeTypes.CHARACTER, 20000),
+    create_node(NodeType.CHARACTER, 20000),
+    create_node(NodeType.GOLD, 11000, gold_value=450),
+    create_node(NodeType.CHARACTER, 20000),
 ]
 
-rows = [row1_nodes, row2_nodes, row3_nodes, row4_nodes, row5_nodes, row6_nodes, row7_nodes, row8_nodes]
+rows = [row0_nodes, row1_nodes, row2_nodes, row3_nodes, row4_nodes, row5_nodes, row6_nodes, row7_nodes]
 
 for i in range(len(rows) - 1):
     create_link(rows[i], "down_right", rows[i + 1])
