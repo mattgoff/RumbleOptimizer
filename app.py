8e75c06569e8416886cf8cb0a15772dcec11f585
Matt Goff <matt@goff.us>
2024-07-22 07:00:20 -0500

    update nodes to have parents and children

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from game_data import ALL_ROWS, TOP_ROW, NodeType
 
 current = TOP_ROW[0]
-
+flattened_graph = [col for row in ALL_ROWS for col in row]
 
 def search(current):
     while current:
@@ -16,6 +16,8 @@ def search(current):
         current = None
 
 
-search(current)
+def find_all_of_type(object_type):
+    return [(object.row, object.col) for object in flattened_graph if object.node_name.name == object_type.name]
 
+nodes_with_type = find_all_of_type(NodeType.GOLD)
 print("here")
--- game_data.py
+++ game_data.py
@@ -10,103 +10,119 @@ class NameAndPriority:
 
 
 class NodeType(enum.Enum):
-    ARCANE_ENERGY = NameAndPriority("arcane_energy", 1)
-    CHARACTER = NameAndPriority("character", 2)
-    TOME = NameAndPriority("tome", 6)
-    CORE = NameAndPriority("core", 10)
-    GOLD = NameAndPriority("gold", 15)
+    ARCANE_ENERGY = NameAndPriority("arcane_energy", 5)
+    CHARACTER = NameAndPriority("character", 4)
+    TOME = NameAndPriority("tome", 3)
+    CORE = NameAndPriority("core", 1)
+    GOLD = NameAndPriority("gold", 2)
 
 
 @dataclass
 class Node:
     node_name: NodeType
     node_cost: int
+    row: int
+    col: int
     gold_value: int = 0
     arcane_value: int = 0
-    node_down_left: Optional['Node'] = None
-    node_down_right: Optional['Node'] = None
+    child_left: Optional['Node'] = None
+    child_right: Optional['Node'] = None
+    parent_left: Optional['Node'] = None
+    parent_right: Optional['Node'] = None
 
 
-def create_node(node_name, node_cost, gold_value=0, arcane_value=0):
-    return Node(node_name, node_cost, gold_value=gold_value, arcane_value=arcane_value)
+def create_node(node_name, node_cost, row, col, gold_value=0, arcane_value=0):
+    return Node(node_name, node_cost, row, col, gold_value=gold_value, arcane_value=arcane_value)
 
 
 def link_nodes(node1, direction, node2):
-    if direction == "down_right":
-        node1.node_down_right = node2
-    elif direction == "down_left":
-        node1.node_down_left = node2
+    if direction == "child_right":
+        node1.child_right = node2
+    if direction == "child_left":
+        node1.child_left = node2
+    if direction == "parent_right":
+        node1.parent_right = node2
+    if direction == "parent_left":
+        node1.parent_left = node2
+
+
+def _link_nodes_according_to_collections(collection1, collection2, dir):
+    for i in range(len(collection1)):
+        link_nodes(collection1[i], dir, collection2[i])
 
 
 def create_link(upper_row, direction, lower_row):
-    length_of_row = len(upper_row) - 1
+    if 'child' in direction:
+        length_of_row = len(upper_row) - 1 if len(upper_row) == 4 else len(upper_row)
+        start = 0 if direction == "child_right" else 1
+        end = length_of_row if direction == "child_right" else length_of_row + 1
+        _link_nodes_according_to_collections(upper_row[start:end], lower_row, direction)
 
-    if length_of_row == 3:
-        if direction == "down_right":
-            for i in range(length_of_row):
-                link_nodes(upper_row[i], direction, lower_row[i])
-        else:
-            for i in range(length_of_row):
-                link_nodes(upper_row[i + 1], direction, lower_row[i])
-    else:
-        if direction == "down_right":
-            for i in range(length_of_row + 1):
-                link_nodes(upper_row[i], direction, lower_row[i + 1])
-        else:
-            for i in range(length_of_row + 1):
-                link_nodes(upper_row[i], direction, lower_row[i])
+    if 'parent' in direction:
+        length_of_row = len(lower_row)
+        start = 0 if direction == "parent_right" else 1
+        end = length_of_row if len(lower_row) == 3 else length_of_row - 2
+        _link_nodes_according_to_collections(lower_row[start:end], upper_row[start:end], direction)
+
 
 
 row0_nodes = [
-    create_node(NodeType.ARCANE_ENERGY, 500, arcane_value=200),
-    create_node(NodeType.CHARACTER, 200),
-    create_node(NodeType.CHARACTER, 200),
-    create_node(NodeType.GOLD, 400, gold_value=75),
+    create_node(NodeType.ARCANE_ENERGY, 500, 0, 0,arcane_value=200),
+    create_node(NodeType.CHARACTER, 200,0 , 1),
+    create_node(NodeType.CHARACTER, 200, 0, 2),
+    create_node(NodeType.GOLD, 400, 0, 3, gold_value=75),
 ]
 row1_nodes = [
-    create_node(NodeType.GOLD, 800, gold_value=150),
-    create_node(NodeType.CHARACTER, 400),
-    create_node(NodeType.TOME, 800),
+    create_node(NodeType.GOLD, 800, 1, 0, gold_value=150),
+    create_node(NodeType.CHARACTER, 400, 1, 1),
+    create_node(NodeType.TOME, 800, 1, 2),
 ]
 row2_nodes = [
-    create_node(NodeType.ARCANE_ENERGY, 2000, arcane_value=250),
-    create_node(NodeType.CHARACTER, 500),
-    create_node(NodeType.CHARACTER, 500),
-    create_node(NodeType.ARCANE_ENERGY, 2000, arcane_value=250),
+    create_node(NodeType.ARCANE_ENERGY, 2000, 2, 0, arcane_value=250),
+    create_node(NodeType.CHARACTER, 500, 2, 1),
+    create_node(NodeType.CHARACTER, 500, 2, 2),
+    create_node(NodeType.ARCANE_ENERGY, 2000, 2, 3, arcane_value=250),
 ]
 row3_nodes = [
-    create_node(NodeType.CHARACTER, 800),
-    create_node(NodeType.CHARACTER, 6000),
-    create_node(NodeType.CHARACTER, 800),
+    create_node(NodeType.CHARACTER, 800,3, 0),
+    create_node(NodeType.CHARACTER, 6000, 3, 1),
+    create_node(NodeType.CHARACTER, 800, 3, 2),
 ]
 row4_nodes = [
-    create_node(NodeType.CORE, 20000),
-    create_node(NodeType.TOME, 4400),
-    create_node(NodeType.CHARACTER, 3500),
-    create_node(NodeType.ARCANE_ENERGY, 6000, arcane_value=500),
+    create_node(NodeType.CORE, 20000, 4, 0),
+    create_node(NodeType.TOME, 4400, 4, 1),
+    create_node(NodeType.CHARACTER, 3500, 4, 2),
+    create_node(NodeType.ARCANE_ENERGY, 6000, 4, 3, arcane_value=500),
 ]
 row5_nodes = [
-    create_node(NodeType.CHARACTER, 2000),
-    create_node(NodeType.GOLD, 4000, gold_value=300),
-    create_node(NodeType.ARCANE_ENERGY, 6000, arcane_value=700),
+    create_node(NodeType.CHARACTER, 2000, 5, 0),
+    create_node(NodeType.GOLD, 4000, 5, 1, gold_value=300),
+    create_node(NodeType.ARCANE_ENERGY, 6000, 5, 2, arcane_value=700),
 ]
 row6_nodes = [
-    create_node(NodeType.CHARACTER, 11000),
-    create_node(NodeType.ARCANE_ENERGY, 11000, arcane_value=1000),
-    create_node(NodeType.CHARACTER, 11000),
-    create_node(NodeType.TOME, 15000),
+    create_node(NodeType.CHARACTER, 11000, 6, 0),
+    create_node(NodeType.ARCANE_ENERGY, 11000, 6, 1, arcane_value=1000),
+    create_node(NodeType.CHARACTER, 11000, 6, 2),
+    create_node(NodeType.TOME, 15000, 6, 3),
 ]
 row7_nodes = [
-    create_node(NodeType.CHARACTER, 20000),
-    create_node(NodeType.GOLD, 11000, gold_value=450),
-    create_node(NodeType.CHARACTER, 20000),
+    create_node(NodeType.CHARACTER, 20000, 7, 0),
+    create_node(NodeType.GOLD, 11000, 7, 1, gold_value=450),
+    create_node(NodeType.CHARACTER, 20000,7 ,2),
 ]
 
 rows = [row0_nodes, row1_nodes, row2_nodes, row3_nodes, row4_nodes, row5_nodes, row6_nodes, row7_nodes]
 
+# create child relationships
 for i in range(len(rows) - 1):
-    create_link(rows[i], "down_right", rows[i + 1])
-    create_link(rows[i], "down_left", rows[i + 1])
+    create_link(rows[i], "child_right", rows[i + 1])
+    create_link(rows[i], "child_left", rows[i + 1])
+
+# create parent relationships
+for i in range(1, len(rows)):
+    create_link(rows[i - 1], "parent_right", rows[i])
+    create_link(rows[i - 1], "parent_left", rows[i])
+
 
 ALL_ROWS = rows
 TOP_ROW = row0_nodes
